7dbe207eda08bc98ca1de479c2e3418001c827d2
Lee Nookx <lnookx@googlemail.com>
2010-12-29 22:27:16 +0000

    Initial application skeleton.

---
+++ src/uk/co/purplemonkeys/gtd/gtd.java
@@ -0,0 +1,13 @@
+package uk.co.purplemonkeys.gtd;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class gtd extends Activity {
+    /** Called when the activity is first created. */
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.main);
+    }
+}
